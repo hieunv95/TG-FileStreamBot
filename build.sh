51ada82ec9174ab8705dfbc0883ba1e989401316
hieunv95 <hieu2395@gmail.com>
2025-03-07 01:25:42 +0000

    fix: path tdlib

--- build.sh
+++ build.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 set -e  # Dừng nếu có lỗi
 
-# Tạo thư mục chứa TDLib
-mkdir -p /tmp/tdlib
-
-# Kiểm tra nếu TDLib chưa tồn tại thì tải về
-if [ ! -f "/tmp/tdlib/lib/libtdjson.so" ]; then
-    echo "⏳ Đang tải TDLib..."
-    curl -L https://github.com/tdlib/td/releases/download/v1.8.0/tdlib-linux-x64.tar.gz | tar -xz -C /tmp/tdlib
-    echo "✅ TDLib đã được cài đặt!"
-fi
-
 # Thiết lập biến môi trường để dùng CGO
 export CGO_ENABLED=1
-export CGO_CFLAGS="-I/tmp/tdlib/include"
-export CGO_LDFLAGS="-L/tmp/tdlib/lib -ltdjson"
+export CGO_CFLAGS="-I/home/vercel/tdlib/include"
+export CGO_LDFLAGS="-L/home/vercel/tdlib/lib -ltdjson"
 
 # Tạo thư mục output cho Vercel
 mkdir -p /vercel/output
