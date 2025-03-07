d14c81b556b214005fa2cfb0e4d0f1529dc14ec4
hieunv95 <hieu2395@gmail.com>
2025-03-07 01:43:17 +0000

    fix: cgo path

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ fi
 
 # Thiết lập biến môi trường để dùng CGO
 export CGO_ENABLED=1
-export CGO_CFLAGS="-I/vercel/tdlib/include"
-export CGO_LDFLAGS="-L/vercel/tdlib/lib -ltdjson"
+export CGO_CFLAGS="-I/vercel/path0/tdlib/include"
+export CGO_LDFLAGS="-L/vercel/path0/tdlib/lib -ltdjson"
 
-ls /vercel/path0
+ls /vercel/path0/tdlib
 
 # Tạo thư mục output cho Vercel
 mkdir -p /vercel/output
